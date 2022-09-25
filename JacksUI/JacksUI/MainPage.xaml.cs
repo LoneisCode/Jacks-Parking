133c20cc3fc55811371430f4a30d0ac675539af9
jfglenn <jessieglenn2002@gmail.com>
2022-09-24 23:29:00 -0500

    working Standard Deviation method, working Confidence Interval method

--- JacksUI/JacksUI/MainPage.xaml.cs
+++ JacksUI/JacksUI/MainPage.xaml.cs
@@ -1,24 +1,46 @@
-﻿
+﻿using System.Linq;
+using System;
+
+
 namespace JacksUI;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	//Input: Array of Red values, Array of Blue values, or Array of Green values
+	//Output: Population Standard Deviation
+    public static double standardDeviation(double[] colorArr, double mean)
+    {
+        double sum = colorArr.Sum(d => Math.Pow(d - mean, 2));
+		return Math.Sqrt((sum) / colorArr.Count());
+		
+    }
+
+	public static double[] ConfidenceInterval(double[] colorArr) {
+		double[] result = new double[2];
+		double mean = colorArr.Average();
+		int sampleSize = colorArr.Length - 1;
+		double sd = standardDeviation(colorArr, mean);
+		double lowerBound = mean - (Constants.confidenceLevel_99 * (sd / Math.Sqrt(sampleSize)));
+		double upperBound = mean + (Constants.confidenceLevel_99 * (sd / Math.Sqrt(sampleSize)));
+
+		result[0] = lowerBound;
+		result[1] = upperBound;
+
+		return result;
 	}
 
 
     private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		double[] colorArr = { 10, 12, 23, 23, 16, 23, 21, 16 };
+
+		CounterBtn.Text = ConfidenceInterval(colorArr).ToString();
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
