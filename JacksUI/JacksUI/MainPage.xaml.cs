d039f91d9f71e4f043359ad9cca255bf8d97ffff
kels <mdkelsey72@gmail.com>
2022-09-26 15:49:47 -0500

    RGB methods for one spot

--- JacksUI/JacksUI/MainPage.xaml.cs
+++ JacksUI/JacksUI/MainPage.xaml.cs
@@ -1,18 +1,81 @@
 ﻿using System.Linq;
 using System;
-
+using System.Drawing;
+//using Android.Graphics;
 
 namespace JacksUI;
 
 public partial class MainPage : ContentPage
 {
+    //System.Drawing.Image lot = new Image { Source = "/Resources/Images/lotImage.jpg" };
+    //Bitmap lotImage = BitmapFactory.DecodeFile("/Resources/Images/lotImage.jpg");
+
+
+    Bitmap lotImage = new Bitmap("/Resources/Images/lotImage.jpg");
+
     public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	//Input: Array of Red values, Array of Blue values, or Array of Green values
-	//Output: Population Standard Deviation
+	
+	//returns the R values of the diagonal pixels. Hardcoded for one spot.
+	public byte[] getR()
+	{
+		int x = 2333;
+		int y = 2297;
+		byte[] rValues = new byte[120];
+
+		for (int i = 0; i < 120; i++)
+		{
+			System.Drawing.Color c = lotImage.GetPixel(x, y);
+			rValues[i] = c.R;
+			x += 2;
+			y -= 2;
+		}
+
+		return rValues;
+	}
+
+    //returns the G values of the diagonal pixels. Hardcoded for one spot.
+    public byte[] getG()
+    {
+        int x = 2333;
+        int y = 2297;
+        byte[] gValues = new byte[120];
+
+        for (int i = 0; i < 120; i++)
+        {
+            System.Drawing.Color c = lotImage.GetPixel(x, y);
+            gValues[i] = c.G;
+            x += 2;
+            y -= 2;
+        }
+
+        return gValues;
+    }
+
+    //returns the B values of the diagonal pixels. Hardcoded for one spot.
+    public byte[] getB()
+    {
+        int x = 2333;
+        int y = 2297;
+        byte[] bValues = new byte[120];
+
+        for (int i = 0; i < 120; i++)
+        {
+            System.Drawing.Color c = lotImage.GetPixel(x, y);
+            bValues[i] = c.B;
+            x += 2;
+            y -= 2;
+        }
+
+        return bValues;
+    }
+
+
+    //Input: Array of Red values, Array of Blue values, or Array of Green values
+    //Output: Population Standard Deviation
     public static double standardDeviation(double[] colorArr, double mean)
     {
         double sum = colorArr.Sum(d => Math.Pow(d - mean, 2));
