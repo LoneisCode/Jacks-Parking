b0d3959492137cf423c1b1f491cbdfd2b89c97c4
kels <mdkelsey72@gmail.com>
2022-09-26 20:07:54 -0500

    Created classes and RGB value logic

---
+++ JacksUI/JacksUI/Bitmapping.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+
+namespace JacksUI
+{
+    internal class Bitmapping
+    {
+        //System.Drawing.Image lot = new Image { Source = "/Resources/Images/lotImage.jpg" };
+        //Bitmap lotImage = BitmapFactory.DecodeFile("/Resources/Images/lotImage.jpg");
+
+        Bitmap lotImage = new Bitmap("/Resources/Images/lotImage.jpg");
+
+        //returns the R values of the diagonal pixels. Hardcoded for one spot.
+        public byte[] getR(int x, int y)
+        {
+            int x = 2333;
+            int y = 2297;
+            byte[] rValues = new byte[120];
+
+            for (int i = 0; i < 120; i++)
+            {
+                System.Drawing.Color c = lotImage.GetPixel(x, y);
+                rValues[i] = c.R;
+                x += 2;
+                y -= 2;
+            }
+
+            return rValues;
+        }
+
+        //returns the G values of the diagonal pixels. Hardcoded for one spot.
+        public byte[] getG()
+        {
+            int x = 2333;
+            int y = 2297;
+            byte[] gValues = new byte[120];
+
+            for (int i = 0; i < 120; i++)
+            {
+                System.Drawing.Color c = lotImage.GetPixel(x, y);
+                gValues[i] = c.G;
+                x += 2;
+                y -= 2;
+            }
+
+            return gValues;
+        }
+
+        //returns the B values of the diagonal pixels. Hardcoded for one spot.
+        public byte[] getB()
+        {
+            int x = 2333;
+            int y = 2297;
+            byte[] bValues = new byte[120];
+
+            for (int i = 0; i < 120; i++)
+            {
+                System.Drawing.Color c = lotImage.GetPixel(x, y);
+                bValues[i] = c.B;
+                x += 2;
+                y -= 2;
+            }
+
+            return bValues;
+        }
+    }
+}
--- JacksUI/JacksUI/MainPage.xaml.cs
+++ JacksUI/JacksUI/MainPage.xaml.cs
@@ -1,111 +1,24 @@
 ﻿using System.Linq;
 using System;
-using System.Drawing;
-//using Android.Graphics;
 
-namespace JacksUI;
-
-public partial class MainPage : ContentPage
+namespace JacksUI
 {
-    //System.Drawing.Image lot = new Image { Source = "/Resources/Images/lotImage.jpg" };
-    //Bitmap lotImage = BitmapFactory.DecodeFile("/Resources/Images/lotImage.jpg");
 
-
-    Bitmap lotImage = new Bitmap("/Resources/Images/lotImage.jpg");
-
-    public MainPage()
+	public partial class MainPage : ContentPage
 	{
-		InitializeComponent();
-	}
-
-	
-	//returns the R values of the diagonal pixels. Hardcoded for one spot.
-	public byte[] getR()
-	{
-		int x = 2333;
-		int y = 2297;
-		byte[] rValues = new byte[120];
-
-		for (int i = 0; i < 120; i++)
+		public MainPage()
 		{
-			System.Drawing.Color c = lotImage.GetPixel(x, y);
-			rValues[i] = c.R;
-			x += 2;
-			y -= 2;
+			InitializeComponent();
 		}
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
 
-		return rValues;
-	}
+			double[] colorArr = { 10, 12, 23, 23, 16, 23, 21, 16 };
 
-    //returns the G values of the diagonal pixels. Hardcoded for one spot.
-    public byte[] getG()
-    {
-        int x = 2333;
-        int y = 2297;
-        byte[] gValues = new byte[120];
+			CounterBtn.Text = Calculations.ConfidenceInterval(colorArr).ToString();
 
-        for (int i = 0; i < 120; i++)
-        {
-            System.Drawing.Color c = lotImage.GetPixel(x, y);
-            gValues[i] = c.G;
-            x += 2;
-            y -= 2;
-        }
-
-        return gValues;
-    }
-
-    //returns the B values of the diagonal pixels. Hardcoded for one spot.
-    public byte[] getB()
-    {
-        int x = 2333;
-        int y = 2297;
-        byte[] bValues = new byte[120];
-
-        for (int i = 0; i < 120; i++)
-        {
-            System.Drawing.Color c = lotImage.GetPixel(x, y);
-            bValues[i] = c.B;
-            x += 2;
-            y -= 2;
-        }
-
-        return bValues;
-    }
-
-
-    //Input: Array of Red values, Array of Blue values, or Array of Green values
-    //Output: Population Standard Deviation
-    public static double standardDeviation(double[] colorArr, double mean)
-    {
-        double sum = colorArr.Sum(d => Math.Pow(d - mean, 2));
-		return Math.Sqrt((sum) / colorArr.Count());
-		
-    }
-
-	public static double[] ConfidenceInterval(double[] colorArr) {
-		double[] result = new double[2];
-		double mean = colorArr.Average();
-		int sampleSize = colorArr.Length - 1;
-		double sd = standardDeviation(colorArr, mean);
-		double lowerBound = mean - (Constants.confidenceLevel_99 * (sd / Math.Sqrt(sampleSize)));
-		double upperBound = mean + (Constants.confidenceLevel_99 * (sd / Math.Sqrt(sampleSize)));
-
-		result[0] = lowerBound;
-		result[1] = upperBound;
-
-		return result;
-	}
-
-
-    private void OnCounterClicked(object sender, EventArgs e)
-	{
-
-		double[] colorArr = { 10, 12, 23, 23, 16, 23, 21, 16 };
-
-		CounterBtn.Text = ConfidenceInterval(colorArr).ToString();
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+			SemanticScreenReader.Announce(CounterBtn.Text);
+		}
 	}
 }
 
