98f16545d56c399e3a42f9519a1a1c6b1ab11899
jfglenn <jessieglenn2002@gmail.com>
2022-09-26 22:11:22 -0500

    installed SkiaSharp nuget package, applied SKBitmap object and SKColor object

--- JacksUI/JacksUI/Bitmapping.cs
+++ JacksUI/JacksUI/Bitmapping.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Drawing;
+using SkiaSharp;
 
 namespace JacksUI
 {
@@ -9,10 +10,12 @@ namespace JacksUI
         //System.Drawing.Image lot = new Image { Source = "/Resources/Images/lotImage.jpg" };
         //Bitmap lotImage = BitmapFactory.DecodeFile("/Resources/Images/lotImage.jpg");
 
-        Bitmap lotImage = new Bitmap("/Resources/Images/lotImage.jpg");
+        SKBitmap lotImage = SKBitmap.Decode("lotImage.jpg");
+
+        //Bitmap lotImage = new Bitmap("/Resources/Images/lotImage.jpg");
 
         //returns the R values of the diagonal pixels. Hardcoded for one spot.
-        public byte[] getR(int x, int y)
+        public byte[] getR()
         {
             int x = 2333;
             int y = 2297;
@@ -20,11 +23,13 @@ namespace JacksUI
 
             for (int i = 0; i < 120; i++)
             {
-                System.Drawing.Color c = lotImage.GetPixel(x, y);
-                rValues[i] = c.R;
+                SKColor c = lotImage.GetPixel(x, y);
+                rValues[i] = c.Red;
                 x += 2;
                 y -= 2;
             }
+
+
 
             return rValues;
         }
@@ -38,8 +43,8 @@ namespace JacksUI
 
             for (int i = 0; i < 120; i++)
             {
-                System.Drawing.Color c = lotImage.GetPixel(x, y);
-                gValues[i] = c.G;
+                SKColor c = lotImage.GetPixel(x, y);
+                gValues[i] = c.Green;
                 x += 2;
                 y -= 2;
             }
@@ -56,8 +61,8 @@ namespace JacksUI
 
             for (int i = 0; i < 120; i++)
             {
-                System.Drawing.Color c = lotImage.GetPixel(x, y);
-                bValues[i] = c.B;
+                SKColor c = lotImage.GetPixel(x, y);
+                bValues[i] = c.Blue;
                 x += 2;
                 y -= 2;
             }
