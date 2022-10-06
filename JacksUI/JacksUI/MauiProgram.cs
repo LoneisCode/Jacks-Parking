8805663ecbf39269ed8a25c25de233497dffe824
kels <mdkelsey72@gmail.com>
2022-10-06 12:16:28 -0500

    Issues with Nuget Package

--- JacksUI/JacksUI/MauiProgram.cs
+++ JacksUI/JacksUI/MauiProgram.cs
@@ -1,18 +1,20 @@
-﻿namespace JacksUI;
-
-public static class MauiProgram
+﻿namespace JacksUI
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
 
-		return builder.Build();
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
+
+			return builder.Build();
+		}
 	}
 }
