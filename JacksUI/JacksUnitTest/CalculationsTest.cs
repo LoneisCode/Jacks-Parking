9abf17576f84add8664e996d25dc854ddec02524
jfglenn <jessieglenn2002@gmail.com>
2022-10-08 23:21:46 -0500

    Created test methods for standardDeviation and confidenceInterval methods, tested for valid array input and null array input

--- JacksUI/JacksUnitTest/CalculationsTest.cs
+++ JacksUI/JacksUnitTest/CalculationsTest.cs
@@ -1,4 +1,5 @@
 using JacksUI;
+using Xunit.Sdk;
 
 namespace JacksUnitTest
 {
@@ -7,8 +8,29 @@ namespace JacksUnitTest
         [Fact]
         public void StandardDeviation()
         {
-            Calculations.standardDeviation(CalculationsHelper.colorArr, CalculationsHelper.colorMean);
+            //valid array input
+            double actualValue = Calculations.standardDeviation(CalculationsHelper.colorArr, CalculationsHelper.colorArrAverage);
+            Assert.Equal(CalculationsHelper.expectedStandardDeviation, actualValue);
 
+            //null array input
+            double[]? colorArr = null;
+            actualValue = Calculations.standardDeviation(colorArr, 0);
+            Assert.Equal(-1, actualValue);
+            
+        }
+
+        [Fact]
+        public void ConfidenceInterval(){
+
+            //valid array input
+            double[] actualValue = Calculations.confidenceInterval(CalculationsHelper.colorArr);
+            Assert.Equal(CalculationsHelper.expectedLowerBound, actualValue[0]);
+            Assert.Equal(CalculationsHelper.expectedUpperBound, actualValue[1]);
+
+            //null array input
+            double[]? colorArr = null;
+            actualValue = Calculations.confidenceInterval(colorArr);
+            Assert.Null(actualValue);
         }
     }
 }
