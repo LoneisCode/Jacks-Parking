04b3c8f7d86b909cf35a777c19b5dbed6b139672
John Workman <workmanjd@jacks.sfasu.edu>
2022-10-10 10:06:08 -0500

    Changed table to allow no nulls and added 3 lots of data

--- dbo.Table.sql
+++ dbo.Table.sql
@@ -1,10 +1,11 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [location] NVARCHAR(50) NOT NULL, 
-    [name] NVARCHAR(50) NOT NULL, 
-    [permits] INT NOT NULL, 
-    [#_of_spaces] INT NOT NULL, 
-    [level] INT NOT NULL, 
-    [building_near] NVARCHAR(50) NULL
-)
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]            INT           NOT NULL,
+    [location]      NVARCHAR (50) NOT NULL,
+    [name]          NVARCHAR (50) NOT NULL,
+    [permits]       INT           NOT NULL,
+    [#_of_spaces]   INT           NOT NULL,
+    [level]         INT           NOT NULL,
+    [building_near] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
