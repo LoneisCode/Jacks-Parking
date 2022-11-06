38fe11af3fc957900b0cde23a35e443acbb5cb2d
jfglenn <jessieglenn2002@gmail.com>
2022-11-06 17:51:49 -0600

    testing bottom row of Village Parking Lot

--- JacksParkingBackEnd/JacksParkingBackEnd/Startup.cs
+++ JacksParkingBackEnd/JacksParkingBackEnd/Startup.cs
@@ -18,20 +18,26 @@ namespace JacksParkingBackEnd
         {
             Configuration = configuration;
 
-            ParkingSpot[] vSpots = new ParkingSpot[3];
+            ParkingSpot[] vSpots = new ParkingSpot[14];
             //when filling, decide if diagonal is top left or bottom right
-            vSpots[0] = new ParkingSpot(379, 328, 362, 356, false); //empty, bottom left
-            vSpots[1] = new ParkingSpot(478, 325, 550, 359, true);//513, 323, 519, 356); old values //red car, top left
-            vSpots[2] = new ParkingSpot(215, 325, 169, 357, false); //white challenger with silver hood
-            Bitmap Village = new Bitmap("Resources/images/lot_image.png");
-            ParkingLot village = new ParkingLot("Resources/images/lot_image.png", vSpots, Village);
+            vSpots[0] = new ParkingSpot(119, 325, 35, 358, false); // Spot 1, empty
+            vSpots[1] = new ParkingSpot(144, 328, 82, 358, false); // Spot 2, empty
+            vSpots[2] = new ParkingSpot(182, 326, 125, 358, false); // Spot 3, taken
+            vSpots[3] = new ParkingSpot(217, 325, 165, 357, false); // Spot 4, taken
+            vSpots[4] = new ParkingSpot(252, 324, 206, 358, false); // Spot 5, taken
+            vSpots[5] = new ParkingSpot(284, 324, 243, 358, false); // Spot 6, taken
+            vSpots[6] = new ParkingSpot(317, 468, 284, 357, false); // Spot 7, taken
+            vSpots[7] = new ParkingSpot(350, 319, 324, 358, false); // Spot 8, taken
+            vSpots[8] = new ParkingSpot(381, 324, 362, 358, false); // Spot 9, empty
+            vSpots[9] = new ParkingSpot(419, 319, 401, 358, false); // Spot 10, taken
+            vSpots[10] = new ParkingSpot(425, 323, 376, 358, true); // Spot 11, empty
+            vSpots[11] = new ParkingSpot(446, 325, 515, 359, true); // Spot 12, empty
+            vSpots[12] = new ParkingSpot(477, 324, 551, 358, true); // Spot 13, taken
+            vSpots[13] = new ParkingSpot(518, 320, 584, 358, true); // Spot 14, taken
+
+            Bitmap Village = new Bitmap("Resources/images/lot_image.jpg");
+            ParkingLot village = new ParkingLot("Resources/images/lot_image.jpg", vSpots, Village);
             System.Diagnostics.Debug.WriteLine(village.SpotsStatus());
-
-
-            //Glenn Test
-            //double[] rValues = Bitmapping.GetR(Village, 379, 328, 362, 356, vSpots[0].GetLength(), 0);
-            //double[] standardOfComparison = Calculations.ConfidenceInterval(rValues);
-            //Calculations.IsAvailable(standardOfComparison, rValues);
 
         }
 
