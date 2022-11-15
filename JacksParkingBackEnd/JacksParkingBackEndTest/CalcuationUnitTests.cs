66ebc6cc475792e166f3fe987133ea873d10c765
jfglenn <jessieglenn2002@gmail.com>
2022-11-14 21:58:46 -0600

    Added unit tests for calculation methods

--- JacksParkingBackEnd/JacksParkingBackEndTest/CalcuationUnitTests.cs
+++ JacksParkingBackEnd/JacksParkingBackEndTest/CalcuationUnitTests.cs
@@ -2,34 +2,46 @@ using JacksParkingBackEnd;
 
 namespace JacksParkingBackEndTest
 {
-    public class CalculationTests
+    public class CalculationUnitTests
     {
+        // No longer testing StandardDeviation() because of its privacy level. 
+        // Because it is a component that ConfidenceInterval() is dependant upon,
+        // it implicitly is tested by testing ConfidenceInterval()
+
+        /*
         [Fact]
         public void StandardDeviation()
         {
-            //valid array input
+            // Valid array input
             double actualValue = Calculations.StandardDeviation(CalculationsHelper.colorArr, CalculationsHelper.colorArrAverage);
             Assert.Equal(CalculationsHelper.expectedStandardDeviation, actualValue);
 
-            //null array input
+            // Null array input
             double[]? colorArr = null;
             actualValue = Calculations.StandardDeviation(colorArr, 0);
             Assert.Equal(-1, actualValue);
 
         }
+        */
 
         [Fact]
-        public void ConfidenceInterval()
+        public void CalculateCorrectConfidenceInterval()
         {
 
             //valid array input
-            double[] actualValue = Calculations.confidenceInterval(CalculationsHelper.colorArr);
+            double[] actualValue = Calculations.ConfidenceInterval(CalculationsHelper.colorArr);
             Assert.Equal(CalculationsHelper.expectedLowerBound, actualValue[0]);
             Assert.Equal(CalculationsHelper.expectedUpperBound, actualValue[1]);
 
+        }
+
+        [Fact]
+        public void CalculateConfidenceIntervalWithNullInput()
+        {
+
             //null array input
             double[]? colorArr = null;
-            actualValue = Calculations.confidenceInterval(colorArr);
+            double[] actualValue = Calculations.ConfidenceInterval(colorArr);
             Assert.Null(actualValue);
         }
     }
