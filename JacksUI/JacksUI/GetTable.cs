a3502cc85f5bc7d170d2a4d528ae6969958280c8
John Workman <workmanjohn@me.com>
2022-11-16 13:04:20 -0600

    prep .7 move

--- JacksUI/JacksUI/GetTable.cs
+++ JacksUI/JacksUI/GetTable.cs
@@ -1,126 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
 
 class GetTable
 {
-    string connString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename = C:\\blabla; Integrated Security = True";
-    SqlConnection conn = new SqlConnection(connString);
-    
     static string getLocation(int id)
     {
-        conn.Open();
-        string query = @"SELECT l.location
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = WebConfigurationManager.ConnectionStrings["JacksParkingConnectionString"].ConnectionString;
 
-        SqlCommand cmd = new SqlCommand(query, conn);
-        SqlDataReader dr = cmd.ExecuteReader();
-        conn.Close();
-        dr.Read();
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "SELECT x_location, y_location FROM ParkingLots";
 
-        string returnVal = dr.GetString();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        sda.SelectCommand = cmd;
 
-        dr.Close();
         
-        return returnVal;
+
+
     }
-
-    static string getName(int id)
-    {
-        conn.Open();
-        string query = @"SELECT l.name
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
-
-        SqlCommand cmd = new SqlCommand(query, conn);
-        conn.Close();
-        SqlDataReader dr = cmd.ExecuteReader();
-        dr.Read();
-
-        string returnVal = dr.GetString();
-
-        dr.Close();
-
-        return returnVal;
-    }
-
-    static string getPermits(int id)
-    {
-        conn.Open();
-        string query = @"SELECT l.permits
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
-
-        SqlCommand cmd = new SqlCommand(query, conn);
-        conn.Close();
-        SqlDataReader dr = cmd.ExecuteReader();
-        dr.Read();
-
-        string returnVal = dr.GetInt();
-
-        dr.Close();
-
-        return returnVal;
-    }
-
-    static string getSpaces(int id)
-    {
-        conn.Open();
-        string query = @"SELECT l.#_of_spaces
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
-
-        SqlCommand cmd = new SqlCommand(query, conn);
-        conn.Close();
-        SqlDataReader dr = cmd.ExecuteReader();
-        dr.Read();
-
-        string returnVal = dr.GetInt();
-
-        dr.Close();
-
-        return returnVal;
-    }
-
-    static string getLevel(int id)
-    {
-        conn.Open();
-        string query = @"SELECT l.level
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
-
-        SqlCommand cmd = new SqlCommand(query, conn)
-        conn.Close();
-        SqlDataReader dr = cmd.ExecuteReader();
-        dr.Read();
-
-        string returnVal = dr.GetInt();
-
-        dr.Close();
-
-        return returnVal;
-    }
-
-    static string getBuilding(int id)
-    {
-        conn.Open();
-        string query = @"SELECT l.building_near
-                        FROM dbo.Table.sql l
-                        WHERE l.Id=" + id + ";";
-
-        SqlCommand cmd = new SqlCommand(query, conn)
-        conn.Close();
-        SqlDataReader dr = cmd.ExecuteReader();
-        dr.Read();
-
-        string returnVal = dr.GetInt();
-
-        dr.Close();
-
-        return returnVal;
-    }
+    
 }
