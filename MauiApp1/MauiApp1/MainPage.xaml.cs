fd5980d52f2629bef74aa9ae1f3bd3bbb755b9ee
LoneisCode <kjislone@gmail.com>
2022-11-16 11:49:10 -0600

    New Solution fix

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        var location = new Location(31.6216, -94.6466);
+
+        MapSpan mapSpan = MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(3));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "Welcome to .NET MAUI!",
+            Location = location,
+        });
+        
+    }
+
+}
+
