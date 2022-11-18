ba44467991a6fc362bbc5a6b7c544447d520d746
LoneisCode <kjislone@gmail.com>
2022-11-17 23:54:27 -0600

    Widget  ready for sql DB when its fucking implemented, but next is adding swipe up and down function to it

---
+++ MauiApp1/MauiApp1/Widget.xaml.cs
@@ -0,0 +1,19 @@
+namespace MauiApp1;
+
+public partial class Widget : ContentView
+{
+	public Widget()
+	{
+		InitializeComponent();
+	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+
+    }
+}
