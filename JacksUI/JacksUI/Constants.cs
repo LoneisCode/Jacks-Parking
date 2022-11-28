9c4995e0fc79cdc27832d02b92c5034ba667505c
mkelsey72 <mdkelsey72@gmail.com>
2022-11-28 12:27:17 -0600

    removed junk

--- JacksUI/JacksUI/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JacksUI
-{
-    public static class Constants
-    {
-        public const double confidenceLevel_99 = 2.576;
-        public const String villageLotImage = "/Resources/Images/lot_image.jpg";
-    }
-}
