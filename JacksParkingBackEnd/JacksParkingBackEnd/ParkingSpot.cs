467d1ec96591b3f7a3946a1e6aeec6ef11156ef9
jfglenn <jessieglenn2002@gmail.com>
2022-11-28 09:13:30 -0600

    added new parkingSpot constructor that takes in all four x,y coordinates of a spot, and finds the min and max x and y values

--- JacksParkingBackEnd/JacksParkingBackEnd/ParkingSpot.cs
+++ JacksParkingBackEnd/JacksParkingBackEnd/ParkingSpot.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Transactions;
 
 namespace JacksParkingBackEnd
 {
     public class ParkingSpot
     {
-        private int topX;
-        private int topY;
-        private int bottomX;
-        private int bottomY;
+        private int topLeftX;
+        private int topLeftY;
+        private int topRightX;
+        private int topRightY;
+        private int bottomLeftX;
+        private int bottomLeftY;
+        private int bottomRightX;
+        private int bottomRightY;
+        //private int bottomX;
+        //private int bottomY;
         private int length;
         private int xRange;
         private double slope;
@@ -17,20 +24,35 @@ namespace JacksParkingBackEnd
         private double[] green;
         private double[] blue;
         private bool isTopLeft;
+        private int maxX;
+        private int maxY;
+        private int minX;
+        private int minY;
 
 
-        public ParkingSpot(int topX, int topY, int bottomX, int bottomY, bool isTopLeft)
+        //public ParkingSpot(int topX, int topY, int bottomX, int bottomY, bool isTopLeft)
+        //{
+        //    this.topX = topX;
+        //    this.bottomY = bottomY;
+        //    this.topY = topY;
+        //    this.bottomX = bottomX;
+        //    xRange = Math.Abs(topX - bottomX);
+        //    // Distance formula
+        //    this.length = (int)Math.Sqrt(Math.Pow((double)(topX - bottomX), 2) + Math.Pow((double)(topY - bottomY), 2)); 
+        //    this.slope = Math.Round(((double)topY - (double)bottomY) / ((double)topX - (double)bottomX), 7);
+        //    this.yIntercept = (this.topY) - (this.slope * this.topX);
+        //    this.isTopLeft = isTopLeft;
+        //}
+
+        public ParkingSpot(int topLeftX, int topLeftY, int topRightX, int topRightY, int bottomLeftX, int bottomLeftY, int bottomRightX, int bottomRightY)
         {
-            this.topX = topX;
-            this.bottomY = bottomY;
-            this.topY = topY;
-            this.bottomX = bottomX;
-            xRange = Math.Abs(topX - bottomX);
-            // Distance formula
-            this.length = (int)Math.Sqrt(Math.Pow((double)(topX - bottomX), 2) + Math.Pow((double)(topY - bottomY), 2)); 
-            this.slope = Math.Round(((double)topY - (double)bottomY) / ((double)topX - (double)bottomX), 7);
-            this.yIntercept = (this.topY) - (this.slope * this.topX);
-            this.isTopLeft = isTopLeft;
+            int[] xCoordinates = {topLeftX, topRightX, bottomLeftX, bottomRightX};
+            int[] yCoordinates = {topLeftY, topRightY, bottomLeftY, bottomRightY};
+
+            this.minX = xCoordinates.Min();
+            this.maxX = xCoordinates.Max();
+            this.minY = yCoordinates.Min();
+            this.maxY = yCoordinates.Max();
         }
 
         public bool GetIsTopLeft()
@@ -57,24 +79,43 @@ namespace JacksParkingBackEnd
             this.blue = blue;
         }
 
-        public int GetBottomX()
+        public int GetBottomLeftX()
         {
-            return this.bottomX;
+            return this.bottomLeftX;
         }
 
-        public int GetBottomY()
+        public int GetBottomLeftY()
         {
-            return this.bottomY;
+            return this.bottomLeftY;
+        }
+        public int GetBottomRightX()
+        {
+            return this.bottomRightX;
         }
 
-        public int GetTopX()
+        public int GetBottomRightY()
         {
-            return this.topX;
+            return this.bottomRightY;
         }
 
-        public int GetTopY()
+        public int GetTopLeftX()
         {
-            return this.topY;
+            return this.topLeftX;
+        }
+
+        public int GetTopLeftY()
+        {
+            return this.topLeftY;
+        }
+
+        public int GetTopRightX()
+        {
+            return this.topRightX;
+        }
+
+        public int GetTopRightY()
+        {
+            return this.topRightY;   
         }
 
         public int GetLength()
@@ -105,6 +146,21 @@ namespace JacksParkingBackEnd
         {
             return this.yIntercept;
         }
-
+        public int GetMinX()
+        {
+            return this.minX;
+        }
+        public int GetMaxX()
+        {
+            return this.maxX;
+        }
+        public int GetMinY()
+        {
+            return this.minY;
+        }
+        public int GetMaxY()
+        {
+            return this.maxY;
+        }
     }
 }
---
+++ JacksParkingBackEnd/JacksParkingBackEnd/RandomizedBitmapping.cs
@@ -0,0 +1,88 @@
+﻿using JacksParkingBackEnd;
+using System;
+using System.Drawing;
+
+public class RandomizedBitmapping
+{
+    // Input: A single parking spot object.
+    // Output: Red component values of the diagonal pixels.
+    // Iterates over the x coordinate.
+    public static double[] GetR(ParkingSpot spot, Bitmap lot)
+    {
+        // TODO: Add "if" to set x and y to top or bottom left (x,y) pair coordinate.
+        // Setting the starting pixel (either top left OR bottom left).
+        // Being int, pixels will be somewhat off of diagonal but should be good enough.
+        int x = spot.GetBottomLeftX();
+        int y = spot.GetBottomLeftY();
+
+        double[] rValues = new double[spot.GetXRange()];
+
+        for (int i = 0; i < spot.GetXRange(); i++)
+        {
+            Color c = lot.GetPixel(x, y);
+            rValues[i] = c.R;
+            if (spot.GetIsTopLeft())
+                x -= 1;
+            else
+                x += 1;
+
+            y = (int)(spot.GetSlope() * x + spot.GetYIntercept());
+        }
+
+        return rValues;
+    }
+
+    // Input: A single parking spot object.
+    // Output: Green component values of the diagonal pixels.
+    // Iterates over the x coordinate.
+    public static double[] GetG(ParkingSpot spot, Bitmap lot)
+    {
+        // TODO: Add "if" to set x and y to top or bottom left (x,y) pair coordinate.
+        // Setting the starting pixel (either top left OR bottom left).
+        // Being int, pixels will be somewhat off of diagonal but should be good enough.
+        int x = spot.GetBottomLeftX();
+        int y = spot.GetBottomLeftY();
+
+        double[] gValues = new double[spot.GetXRange()];
+
+        for (int i = 0; i < spot.GetXRange(); i++)
+        {
+            Color c = lot.GetPixel(x, y);
+            gValues[i] = c.G;
+            if (spot.GetIsTopLeft())
+                x -= 1;
+            else
+                x += 1;
+            y = (int)(spot.GetSlope() * x + spot.GetYIntercept());
+        }
+
+        return gValues;
+    }
+
+    // Input: A single parking spot object.
+    // Output: Green component values of the diagonal pixels.
+    // Iterates over the x coordinate.
+    public static double[] GetB(ParkingSpot spot, Bitmap lot)
+    {
+        // TODO: Add "if" to set x and y to top or bottom left (x,y) pair coordinate.
+        // Setting the starting pixel (either top left OR bottom left).
+        // Being int, pixels will be somewhat off of diagonal but should be good enough.
+        int x = spot.GetBottomLeftX();
+        int y = spot.GetBottomLeftY();
+
+        double[] bValues = new double[spot.GetXRange()];
+
+        for (int i = 0; i < spot.GetXRange(); i++)
+        {
+            Color c = lot.GetPixel(x, y);
+            bValues[i] = c.B;
+            if (spot.GetIsTopLeft())
+                x -= 1;
+            else
+                x += 1;
+            y = (int)(spot.GetSlope() * x + spot.GetYIntercept());
+        }
+
+        return bValues;
+    }
+}
