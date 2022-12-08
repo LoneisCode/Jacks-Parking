e9d43e57ab8d7a6d7736cf9f69e4a971e4bec7d3
jfglenn <jessieglenn2002@gmail.com>
2022-12-08 14:16:11 -0600

    added sqlite connection

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
+using Microsoft.Data.Sqlite;
 
 namespace MauiApp1;
 
@@ -14,6 +15,11 @@ public partial class MainPage : ContentPage
     protected override void OnNavigatedTo(NavigatedToEventArgs args)
     {
         base.OnNavigatedTo(args);
+
+        SqliteCommection sqliteConn;
+        sqliteConn = Accessdbfe.CreateConnection();
+        sqliteConn.Open();
+        string output = Accessdbfe.ReadData(sqliteConn, 1);
 
         var sfa = new Location(31.6216, -94.6466);
         var villageLot = new Location(31.616634, -94.650789);
